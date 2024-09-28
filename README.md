744bf2b53ba4de4fb98f47050e277ceaffdfba53
rahul7310 <rahulprasanna73@gmail.com>
2024-09-28 17:06:35 -0600

    Changes to make navbar and team page mobile friendly

--- README.md
+++ README.md
@@ -1,7 +1,27 @@
 # Stellar_Mapping
+
 Predicting and classifying constellations in space.
 
 ## Team Members
+
 1. Rahul Prasanna
 2. Sivakumar Ramakrishnan
 3. Sai Nandini Tata
+
+## Project Setup
+
+Make sure you have node installed. Clone the repository and open the project directory in your terminal and execute the following commands
+
+```
+npm install
+```
+
+```
+npm run dev
+```
+
+You should now be able to view the website on your local host.
+
+## Attributions
+
+Moon by Poly by Google [CC-BY](https://creativecommons.org/licenses/by/3.0/) via [Poly Pizza](https://poly.pizza/m/9OPocAqXM0u)
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,79 +1,187 @@
+import {
+  AppBar,
+  Box,
+  Button,
+  Divider,
+  Drawer,
+  IconButton,
+  ListItemButton,
+  ListItemText,
+  Toolbar,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
+import { useState } from "react";
 export default function Navbar({ setSelectedTab, selectedTab }) {
-  console.log();
-  return (
-    <div
-      style={{
-        position: "absolute",
-        top: 0,
-        left: 0,
-        width: "100%",
-      }}
-    >
-      <div
-        style={{
-          margin: "20px",
-          display: "flex",
-          color: "ivory",
-          justifyContent: "space-evenly",
-        }}
-      >
-        <div
-          style={{
-            cursor: "pointer",
-            border:
-              selectedTab === "Introduction" ? "solid 1px yellow" : "none",
+  const [open, setOpen] = useState(false);
+  const toggleDrawer = (open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+    setOpen(open);
+  };
 
-            borderRadius: "5px",
-          }}
-          onClick={() => setSelectedTab("Introduction")}
+  return (
+    <AppBar
+      style={{ backgroundColor: "black", height: "10px", zIndex: "1" }}
+      position="static"
+    >
+      <Toolbar>
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="open drawer"
+          sx={{ mr: 2, display: { xs: "block", sm: "flex", md: "none" } }}
+          onClick={toggleDrawer(true)}
         >
-          <div style={{ margin: "5px" }}>Introduction</div>
-        </div>
+          <MenuIcon />
+        </IconButton>
         <div
           style={{
-            cursor: "pointer",
-            border:
-              selectedTab === "Data Exploration" ? "solid 1px yellow" : "none",
-            borderRadius: "5px",
+            display: "flex",
+            justifyContent: "space-evenly",
+            width: "100%",
           }}
-          onClick={() => setSelectedTab("Data Exploration")}
         >
-          <div style={{ margin: "5px" }}>Data Exploration</div>
+          <Button
+            onClick={() => setSelectedTab("Introduction")}
+            color="inherit"
+            sx={{
+              display: { xs: "none", md: "block" },
+              border:
+                selectedTab === "Introduction" ? "solid 1px yellow" : "none",
+            }}
+          >
+            Introduction
+          </Button>
+          <Button
+            onClick={() => setSelectedTab("Data Exploration")}
+            color="inherit"
+            sx={{
+              display: { xs: "none", md: "block" },
+              border:
+                selectedTab === "Data Exploration"
+                  ? "solid 1px yellow"
+                  : "none",
+            }}
+          >
+            Data Exploration
+          </Button>
+          <Button
+            onClick={() => setSelectedTab("Model Implemented")}
+            color="inherit"
+            sx={{
+              display: { xs: "none", md: "block" },
+              border:
+                selectedTab === "Model Implemented"
+                  ? "solid 1px yellow"
+                  : "none",
+            }}
+          >
+            Model Implemented
+          </Button>
+          <Button
+            onClick={() => setSelectedTab("Conclusion")}
+            color="inherit"
+            sx={{
+              display: { xs: "none", md: "block" },
+              border:
+                selectedTab === "Conclusion" ? "solid 1px yellow" : "none",
+            }}
+          >
+            Conclusion
+          </Button>
+          <Button
+            onClick={() => setSelectedTab("Team")}
+            color="inherit"
+            sx={{
+              display: { xs: "none", md: "block" },
+              border: selectedTab === "Team" ? "solid 1px yellow" : "none",
+            }}
+          >
+            Team
+          </Button>
         </div>
-        <div
-          style={{
-            cursor: "pointer",
-            border:
-              selectedTab === "Models Implemented"
-                ? "solid 1px yellow"
-                : "none",
-            borderRadius: "5px",
-          }}
-          onClick={() => setSelectedTab("Models Implemented")}
-        >
-          <div style={{ margin: "5px" }}>Models Implemented</div>
-        </div>
-        <div
-          style={{
-            cursor: "pointer",
-            border: selectedTab === "Conclusion" ? "solid 1px yellow" : "none",
-            borderRadius: "5px",
-          }}
-          onClick={() => setSelectedTab("Conclusion")}
-        >
-          <div style={{ margin: "5px" }}>Conclusion</div>
-        </div>
-        <div
-          style={{
-            cursor: "pointer",
-            border: selectedTab === "Team" ? "solid 1px yellow" : "none",
-            borderRadius: "5px",
-          }}
-          onClick={() => setSelectedTab("Team")}
-        >
-          <div style={{ margin: "5px" }}>Team</div>
-        </div>
-      </div>
-    </div>
+
+        <Drawer anchor="left" open={open} onClose={toggleDrawer(false)}>
+          <Box
+            sx={{
+              p: 2,
+              height: 1,
+              backgroundColor: "black",
+              color: "ivory",
+            }}
+          >
+            <IconButton onClick={toggleDrawer(false)} sx={{ mb: 2 }}>
+              <CloseIcon />
+            </IconButton>
+            <Divider sx={{ mb: 2 }} />
+            <Box sx={{ mb: 2 }}>
+              <ListItemButton>
+                <ListItemText
+                  onClick={() => {
+                    setOpen(false);
+                    setSelectedTab("Introduction");
+                  }}
+                  style={{ color: selectedTab === "Introduction" && "yellow" }}
+                  primary="Introduction"
+                />
+              </ListItemButton>
+              <ListItemButton
+                onClick={() => {
+                  setOpen(false);
+                  setSelectedTab("Data Exploration");
+                }}
+              >
+                <ListItemText
+                  style={{
+                    color: selectedTab === "Data Exploration" && "yellow",
+                  }}
+                  primary="Data Exploration"
+                />
+              </ListItemButton>
+              <ListItemButton
+                onClick={() => {
+                  setOpen(false);
+                  setSelectedTab("Model Implemented");
+                }}
+              >
+                <ListItemText
+                  style={{
+                    color: selectedTab === "Model Implemented" && "yellow",
+                  }}
+                  primary="Model Implemented"
+                />
+              </ListItemButton>
+              <ListItemButton
+                onClick={() => {
+                  setOpen(false);
+                  setSelectedTab("Conclusion");
+                }}
+              >
+                <ListItemText
+                  style={{ color: selectedTab === "Conclusion" && "yellow" }}
+                  primary="Conclusion"
+                />
+              </ListItemButton>
+              <ListItemButton
+                onClick={() => {
+                  setOpen(false);
+                  setSelectedTab("Team");
+                }}
+              >
+                <ListItemText
+                  style={{ color: selectedTab === "Team" && "yellow" }}
+                  primary="Team"
+                />
+              </ListItemButton>
+            </Box>
+          </Box>
+        </Drawer>
+      </Toolbar>
+    </AppBar>
   );
 }
