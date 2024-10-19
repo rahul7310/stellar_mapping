933085d8ec2b5f17453fecb83850d0b2491f16aa
rahul7310 <rahulprasanna73@gmail.com>
2024-10-18 23:12:30 -0600

    Some restructuring in the datasets folder

--- src/Components/DataExploration.jsx
+++ src/Components/DataExploration.jsx
@@ -14,19 +14,19 @@ import Gemini from "../../datasets/constellation_templates/Gemini.jpg";
 import Leo from "../../datasets/constellation_templates/Leo.jpg";
 import Orion from "../../datasets/constellation_templates/Orion.jpg";
 
-import Andromeda_stellarium from "../../datasets/stellarium_constellations/Andromeda001.png";
-import Aries_stellarium from "../../datasets/stellarium_constellations/Aries001.png";
-import Bootes_stellarium from "../../datasets/stellarium_constellations/Bootes001.png";
-import Gemini_stellarium from "../../datasets/stellarium_constellations/Gemini001.png";
-import Leo_stellarium from "../../datasets/stellarium_constellations/Leo001.png";
-import Orion_stellarium from "../../datasets/stellarium_constellations/Orion001.png";
+import Andromeda_stellarium from "../../datasets/stellarium_constellations/sample/Andromeda001.png";
+import Aries_stellarium from "../../datasets/stellarium_constellations/sample/Aries001.png";
+import Bootes_stellarium from "../../datasets/stellarium_constellations/sample/Bootes001.png";
+import Gemini_stellarium from "../../datasets/stellarium_constellations/sample/Gemini001.png";
+import Leo_stellarium from "../../datasets/stellarium_constellations/sample/Leo001.png";
+import Orion_stellarium from "../../datasets/stellarium_constellations/sample/Orion001.png";
 
-import img1 from "../../datasets/roboflow_constellation_images/train/2022-01-08-00-00-00-n_png_jpg.rf.5ad2e498f3937e5fc7f746ff4954afca.jpg";
-import img2 from "../../datasets/roboflow_constellation_images/train/2022-01-10-00-00-00-s_png_jpg.rf.9949ec204bd5b8af72cf2cb22a3402b0.jpg";
-import img3 from "../../datasets/roboflow_constellation_images/train/2022-01-15-00-00-00-s_png_jpg.rf.1c04821ef762c0af4d5bf8772e6ac08d.jpg";
-import img4 from "../../datasets/roboflow_constellation_images/train/2022-01-22-00-00-00-s_png_jpg.rf.37e31378be869cbc5da12083a9452f1c.jpg";
-import img5 from "../../datasets/roboflow_constellation_images/train/2022-01-30-00-00-00-s_png_jpg.rf.b5eee58c97e33ef80b95c30a8aad3d23.jpg";
-import img6 from "../../datasets/roboflow_constellation_images/train/2022-01-30-00-00-00-s_png_jpg.rf.b5eee58c97e33ef80b95c30a8aad3d23.jpg";
+import img1 from "../../datasets/roboflow_constellation_images/sample/2022-01-08-00-00-00-n_png_jpg.rf.5ad2e498f3937e5fc7f746ff4954afca.jpg";
+import img2 from "../../datasets/roboflow_constellation_images/sample/2022-01-10-00-00-00-s_png_jpg.rf.9949ec204bd5b8af72cf2cb22a3402b0.jpg";
+import img3 from "../../datasets/roboflow_constellation_images/sample/2022-01-15-00-00-00-s_png_jpg.rf.1c04821ef762c0af4d5bf8772e6ac08d.jpg";
+import img4 from "../../datasets/roboflow_constellation_images/sample/2022-01-22-00-00-00-s_png_jpg.rf.37e31378be869cbc5da12083a9452f1c.jpg";
+import img5 from "../../datasets/roboflow_constellation_images/sample/2022-01-30-00-00-00-s_png_jpg.rf.b5eee58c97e33ef80b95c30a8aad3d23.jpg";
+import img6 from "../../datasets/roboflow_constellation_images/sample/2022-01-30-00-00-00-s_png_jpg.rf.b5eee58c97e33ef80b95c30a8aad3d23.jpg";
 
 export default function DataExploration() {
   const template_images_list = [
